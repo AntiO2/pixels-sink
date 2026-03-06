9185f56ec5772136e4c797389353df68890a813c
AntiO2 <antio2@qq.com>
2026-03-06 12:12:22 +0800

    Update Flink Config

--- src/main/java/io/pixelsdb/pixels/sink/config/PixelsSinkConstants.java
+++ src/main/java/io/pixelsdb/pixels/sink/config/PixelsSinkConstants.java
@@ -25,7 +25,7 @@ public final class PixelsSinkConstants
     public static final String ROW_RECORD_KAFKA_PROP_FACTORY = "row-record";
     public static final String TRANSACTION_KAFKA_PROP_FACTORY = "transaction";
     public static final int MONITOR_NUM = 2;
-    public static final int MAX_QUEUE_SIZE = 100_000;
+    public static final int MAX_QUEUE_SIZE = 1_000;
     public static final String SNAPSHOT_TX_PREFIX = "SNAPSHOT-";
 
     private PixelsSinkConstants()
